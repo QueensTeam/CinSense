df5f0b82dfbc8dc9643dadafc5a7c3192a22c70b
Anastasija Vasiljeva <anastasija.vasiljeva@stud.vgtu.lt>
2021-04-15 20:35:51 +0300

    Added database connection methods

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 from flask import Flask, render_template
+import requests
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def getAllMovies():
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=cff76c61f7621b5ebb8bdf5b5fe35694&sort_by=popularity.desc&page=1")
+    print(response.json())
+
+def getGenres():
+    response = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=cff76c61f7621b5ebb8bdf5b5fe35694&language=en-US")
+    print(response.json())
+
+def connectToDB():
+    myconn = mysql.connector.connect(host = HOSTNAME, user = "root",passwd = "")  
+    print(myconn)  
 
 app = Flask(__name__)
 
