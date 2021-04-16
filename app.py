d387277e836eb97049a4b1e4b43c57825970d505
Anastasija Vasiljeva <anastasija.vasiljeva@stud.vgtu.lt>
2021-04-16 12:11:30 +0300

    Basic registration & authorization

--- app.py
+++ app.py
@@ -2,21 +2,54 @@ from flask import Flask, render_template
 import requests
 import mysql.connector
 from dotenv import load_dotenv
+import os
+from hashlib import sha256
+import pymysql
 
 load_dotenv()
 
 def getAllMovies():
-    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=cff76c61f7621b5ebb8bdf5b5fe35694&sort_by=popularity.desc&page=1")
-    print(response.json())
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key={}&sort_by=popularity.desc&page=1".format(TMDB_API_KEY))
+    return response.json()
 
 def getGenres():
-    response = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=cff76c61f7621b5ebb8bdf5b5fe35694&language=en-US")
+    response = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=" + os.environ.get('TMDB_API_KEY') + "&language=en-US")
     print(response.json())
 
 def connectToDB():
-    myconn = mysql.connector.connect(host = HOSTNAME, user = "root",passwd = "")  
-    print(myconn)  
+    connection = pymysql.connect(host='localhost', user='root', password='', database='cinsense', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    return connection
+    
+def registerUser(login, email, password):
+    conn = connectToDB()
+    cursor = conn.cursor()
+    if (checkUsernameUniqueness(login) == 0):
+        password_salted = os.environ.get('SALT') + password
+        psw_hash = sha256(password_salted.encode('utf-8')).hexdigest()
+        cursor.execute("INSERT INTO user (username, email, password) VALUES ('" + login + "', '" + email + "', '" + psw_hash + "')")
+        conn.commit()
+        conn.close()
+    else:
+        print('This user already exists')
 
+def checkUsernameUniqueness(login):
+    conn = connectToDB()
+    cursor = conn.cursor()    
+    result = cursor.execute("SELECT * FROM user WHERE username='" + login + "'")
+    conn.close()
+    return result
+
+def loginUserWithUName(login, password):
+    conn = connectToDB()
+    cursor = conn.cursor()    
+    password_salted = os.environ.get('SALT') + password
+    psw_hash = sha256(password_salted.encode('utf-8')).hexdigest()
+    result = cursor.execute("SELECT * FROM user WHERE username='" + login + "' and password='" + psw_hash + "'")
+    if (result == 0):
+        print('Error occured: password and username do not match')
+    else:
+        print('You lucky boi, go on')
+    
 app = Flask(__name__)
 
 @app.route("/index.html")
