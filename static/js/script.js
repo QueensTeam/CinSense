2b66de933547475b430e5159d2c907aff6aa3ddf
doizzie <d.lupeikyte@gmail.com>
2021-04-17 11:38:39 +0300

    4vers.
    Updatintas recomendacijos langas, about langas, pridėtas favicon, etc.

--- static/js/script.js
+++ static/js/script.js
@@ -1,90 +1,18 @@
-const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
-let currentPage = 1;
-let totalMovies = 0;
-let currentUrl = "/getAll/";
-
 $(document).ready(function () {
-  loadMovies(currentPage, currentUrl);
+
   $('.first-button').on('click', function () {
+
     $('.animated-icon1').toggleClass('open');
   });
   $('.second-button').on('click', function () {
+
     $('.animated-icon2').toggleClass('open');
   });
   $('.third-button').on('click', function () {
+
     $('.animated-icon3').toggleClass('open');
   });
 });
-
-window.addEventListener('scroll', () => {
-  const {
-      scrollTop,
-      scrollHeight,
-      clientHeight
-  } = document.documentElement;
-
-  if (scrollTop + clientHeight >= scrollHeight && currentPage <= 500) {
-      currentPage++;
-      loadMovies(currentPage, currentUrl);
-      console.log(currentPage);
-  }
-}, {
-  passive: true
-});
-
-const hasMoreMovies = (page, total) => {
-  const startIndex = (page - 1) * 20 + 1;
-  return total === 0 || startIndex < total;
-};
-
-const addMovies = (movies) => {
-  mv = JSON.parse(movies);
-  mv.forEach(addMovie);
-};
-
-function searchForMovies(){
-  var genre = document.getElementById("genre").value;
-  var decade = document.getElementById("decade").value;
-  var voteavg = document.getElementById("average").value;
-  var votecnt = document.getElementById("votecount").value;
-  console.log(decade + " " + voteavg + " " + votecnt);
-  document.getElementById("gallery").innerHTML = "";
-  url = "/getAll/" + genre + "/";
-  console.log(currentPage);
-  currentUrl = url;
-  loadMovies(currentPage, url);
-}
-
-
-function addMovie(value){
-    if (value.hasOwnProperty('release_date')) {
-      release_year = value.release_date.substring(0, 4);
-    }
-    else {
-      release_year = "-";
-    }
-	  alt = value.title + " (" +  release_year+ ")";
-    title = value.title;
-    console.log(title);
-    if (value.hasOwnProperty('poster_path')){
-      movie = "<img src=\"https://image.tmdb.org/t/p/w600_and_h900_bestv2/" + value.poster_path.substring(1) + "\" alt=\"" + alt + "\" title=\"" + title + "\" class=\"col-12 col-lg-2  mov\"/>";
-    }
-      $("#gallery").append(movie);
-}
-
-const loadMovies = async (page, url) => {
-  try {
-          console.log(url);
-          const response = await $.get(url + page);
-          console.log(response);
-          addMovies(response);
-          totalMovies = response.totalMovies;
-  } catch (error) {
-      console.log(error.message);
-  } finally {
-  }
-};
 
 window.transitionToPage = function(href) {
     document.querySelector('body').style.opacity = 0
@@ -159,4 +87,54 @@ function openRegister(){
   logForm.classList.add("dontshow");
   // regForm.classList.add("show");
   regButton.classList.add("dontshow");
+}
+
+function checkWatchlist(){
+  var checkButton = document.getElementById("checkedWatchlist");
+  var uncheckButton = document.getElementById("uncheckedWatchlist");
+  if(checkButton.classList.contains("dontshow")){
+    checkButton.classList.remove("dontshow");
+  }
+  uncheckButton.classList.add("dontshow");
+}
+
+function uncheckWatchlist(){
+  var checkButton = document.getElementById("checkedWatchlist");
+  var uncheckButton = document.getElementById("uncheckedWatchlist");
+  if(uncheckButton.classList.contains("dontshow")){
+    uncheckButton.classList.remove("dontshow");
+  }
+  checkButton.classList.add("dontshow");
+}
+function checkSeen(){
+  var checkButton = document.getElementById("checkedSeen");
+  var uncheckButton = document.getElementById("uncheckedSeen");
+  if(checkButton.classList.contains("dontshow")){
+    checkButton.classList.remove("dontshow");
+  }
+  uncheckButton.classList.add("dontshow");
+}
+
+function uncheckSeen(){
+  var checkButton = document.getElementById("checkedSeen");
+  var uncheckButton = document.getElementById("uncheckedSeen");
+  if(uncheckButton.classList.contains("dontshow")){
+    uncheckButton.classList.remove("dontshow");
+  }
+  checkButton.classList.add("dontshow");
+}
+
+function showTheFooter(){
+  var foot1 = document.getElementById("footerInfo1");
+  var foot2 = document.getElementById("footerInfo2");
+  var cred = document.getElementById("creatorNames");
+  if(foot2.classList.contains("dontshow")){
+    foot2.classList.remove("dontshow");
+    cred.classList.remove("dontshow");
+    foot1.classList.add("dontshow");
+  } else if (foot1.classList.contains("dontshow")){
+    foot1.classList.remove("dontshow");
+    foot2.classList.add("dontshow");
+    cred.classList.add("dontshow");
+  }
 }
